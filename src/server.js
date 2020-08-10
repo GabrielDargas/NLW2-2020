cd27b7d2b79f685f0355408fc5acaed82eb7f8e4
GabrielDargas <68109894+GabrielDargas@users.noreply.github.com>
2020-08-10 20:55:49 -0300

    Aula 05

--- src/server.js
+++ src/server.js
@@ -1,94 +1,13 @@
-//Dados
-const proffys = [
-    {
-        name: "Diego Fernandes",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp: "92929292",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", subject: "Química",
-        cost: "20",
-        weekday: [0],
-        time_from: [720],
-        time_to: [1220]
-    },
-    {
-        name: "Daniele Evangelista",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp: "92929292",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", subject: "Química",
-        cost: "20",
-        weekday: [1],
-        time_from: [720],
-        time_to: [1220]
-    },
-    {
-        name: "Marcelo o outro",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp: "92929292",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", subject: "Química",
-        cost: "20",
-        weekday: [1],
-        time_from: [720],
-        time_to: [1220]
-    }
-]
-
-const subjects = [
-    "Artes",
-    "Biologia",
-    "Ciências",
-    "Educação física",
-    "Física",
-    "Geografia",
-    "História",
-    "Matemática",
-    "Português",
-    "Química",
-]
-const weekdays = [
-    "Domingo",
-    "Segunda-feira",
-    "Terça-feira",
-    "Quarta-feira",
-    "Quinta-feira",
-    "Sexta-feira",
-    "Sábado",
-]
-
-function getSubject(subjectNumber){
-    const arrayPosition = +subjectNumber - 1
-    return subjects[arrayPosition]
-}
-
-
-function pageLanding(req, res) {
-    return res.render("index.html")
-}
-//Funcionalidades
-function pageStudy(req, res) {
-    const filters = req.query
-    return res.render("study.html", { proffys, filters, subjects, weekdays })
-}
-
-function pageGiveClasses(req, res) {
-    const data = req.query
-
-    const isNotEmpty = Object.keys(data).length != 0
-    if(isNotEmpty){
-        data.subject = getSubject (data.subject)
-
-        proffys.push(data)
-
-        return res.redirect("/study")
-    }
-
-    //adicionar ao proffy
-    return res.render("give-classes.html", {subjects, weekdays})
-}
-
 //Servidor
 const express = require('express')
 const server = express()
 const nunjucks = require('nunjucks')
+const {
+    pageLanding,
+    pageStudy,
+    pageGiveClasses,
+    saveClasses
+} = require('./pages')
 
 //configurar nunjucks
 nunjucks.configure('src/views', {
@@ -98,10 +17,13 @@ nunjucks.configure('src/views', {
 })
 
 server
+    //receber os dados do req body
+    .use(express.urlencoded({ extended: true}))
     //configurar arquivos estáticos
     .use(express.static("public"))
     //rotas da aplicação
     .get("/", pageLanding)
     .get("/study", pageStudy)
     .get("/give-classes", pageGiveClasses)
+    .post("/save-classes", saveClasses)
     .listen(5500)
